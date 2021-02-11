cbac3fb9ef03a2e463d7b971e5ba5fe16671be3d
Ivano Carvalho <ivano.sistemas@gmail.com>
2021-02-11 02:35:16 -0300

    challenge reactjs-concepts repository crud

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [ repositories, setRepository ] = useState([]);
+
+  useEffect(() => {
+
+    load();
+
+  }, []);
+
+  function load() {
+    api.get('/repositories')
+    .then( response => {
+      setRepository( response.data );
+    })
+  }
+
+
   async function handleAddRepository() {
-    // TODO
+    
+    const response = await api.post( '/repositories', {
+      title: `Novo repositorio ${Date.now()}`,
+      "url" : "www.hotmail.com",
+      "techs" : "['delphi', 'java', 'React]"
+    });
+
+    const repo = response.data;
+    setRepository( [...repositories, repo ]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    const repoIndex = repositories.findIndex( repository => repository.id === id );
+
+    if ( repoIndex >= 0 ) {
+
+      const response = await api.delete(`/repositories/${id}`);
+      const { status } = response;
+
+      if ( status === 204 ) {
+
+        repositories.splice( repoIndex, 1 );
+        setRepository([ ...repositories ]);
+
+      }
+
+    }
+   
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {  
+          repositories.map( repo => {
+              return <li key={repo.id}>
+                      {repo.title}
+                      <button onClick={() => handleRemoveRepository(repo.id)}>
+                        Remover
+                      </button>
+                      </li>
+          })
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
